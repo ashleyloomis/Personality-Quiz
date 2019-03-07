9d5f508da1d69e13be2396abcecd24d89537ad48
student7 <student7@student7s-macbook-air.local>
2019-03-07 11:38:29 -0600

    Initial Commit

---
+++ Personality Quiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Personality Quiz
+//
+//  Created by student7 on 3/7/19.
+//  Copyright © 2019 student7. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
